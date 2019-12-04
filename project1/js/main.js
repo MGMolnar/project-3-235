adcc46039410267abfc20e81dcffc01120c00073
MGMolnar <55547115+MGMolnar@users.noreply.github.com>
2019-12-04 17:57:19 -0500

    added some functions to be used for later
    functions created

--- project1/js/main.js
+++ project1/js/main.js
@@ -10,10 +10,17 @@ let controlScreen;
 let bigSquirmle;
 let stage;
 
+//used to create the four different scenes in the scene.
 function createPages(){
 
+    //creates the titleScreen and adds it to the stage
     titleScreen = new PIXI.Container();
     stage.appendChild(titleScreen);
+    
+    //a function that will put all the text,
+    //objects, or buttons onto the title screen
+    settupTitleScreen();
+
 
     gameScreen = new PIXI.Container();
     gameScreen.visible = false;
@@ -27,5 +34,29 @@ function createPages(){
     endScreen.visible = false;
     stage.addchild(endScreen);a
 
+
+}
+
+//will create all the text, buttons, and objects
+//onto the title screen
+function settupTitleScreen(){
+
+}
+
+//will create all the text, buttons, and objects
+//onto the game screen
+function settupGameScreen(){
+
+}
+
+//will create all the text, buttons, and objects
+//onto the end screen
+function settupEndScreen(){
+
+}
+
+//will create all the text, buttons, and objects
+//onto the control screen
+function settupControlScreen(){
 
 }
